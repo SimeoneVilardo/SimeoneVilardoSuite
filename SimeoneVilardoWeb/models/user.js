415cfb22e581625541b64e88bfa249e8972763d0
SimeoneVilardo <simeone.vilardo@gmail.com>
2017-04-04 21:22:45 +0200

    Replace bcrypt with bcrypt-nodejs (linux - raspberry compatibility) Remove visual studio files

--- SimeoneVilardoWeb/models/user.js
+++ SimeoneVilardoWeb/models/user.js
@@ -1,5 +1,5 @@
 ﻿var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 mongoose.Promise = require('bluebird');
 var Schema = mongoose.Schema;
 

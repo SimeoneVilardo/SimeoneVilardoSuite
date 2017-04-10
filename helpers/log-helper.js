c07eff3ed7bf66aa59c44beb1c9ae099896dad59
Simeone Vilardo <simeone.vilardo@gmail.com>
2017-04-10 22:53:35 +0200

    Small cleanup

--- helpers/log-helper.js
+++ helpers/log-helper.js
@@ -4,7 +4,7 @@ var winston = require('winston');
 
 logHelper.loggerConfig = function () {
     var logConfig = {
-        transports: [ (!process.env.ENV || process.env.ENV === 'development')? new winston.transports.Console({colorize: true}) : new winston.transports.File({
+        transports: [ (!process.env.ENV || process.env.ENV === 'development') ? new winston.transports.Console({colorize: true}) : new winston.transports.File({
             filename: path.join(__dirname, '..', 'logs', 'simeonevilardo.log')
         })],
             level: process.env.LOG_LEVEL || 'info',
